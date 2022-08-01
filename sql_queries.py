44621eebc5e5553fc152d547449e58bd54859dd4
pgarduque <pgarduque@yahoo.com>
2022-07-31 17:22:45 -0700

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -32,8 +32,7 @@ VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)
 user_table_insert = ("""INSERT INTO users(user_id,first_name,last_name,level,gender)
 VALUES(%s,%s,%s,%s,%s)
 ON CONFLICT (user_id)
-DO UPDATE
-    SET level = EXCLUDED.level;
+DO UPDATE SET level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""INSERT INTO songs(song_id,title,artist_id,year,duration)
